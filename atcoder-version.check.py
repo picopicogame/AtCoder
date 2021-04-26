4416b72da7c45bd7acc3bceca108f22379681da4
picopicogame <picopico_famicom@yahoo.co.jp>
2021-04-26 18:18:18 +0900

    フォルダ作成、ライブラリのインストールなど初期設定を完了

---
+++ atcoder-version.check.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    各ライブラリのバージョンをチェックするコード
+    https://note.nkmk.me/atcoder-python-numpy-scipy-version/より
+
+"""
+
+import platform
+import numpy
+import scipy
+
+print('Python:       ', platform.python_version())
+print('NumPy:        ', numpy.__version__)
+print('SciPy:        ', scipy.__version__)
+
+try:
+    import sklearn
+    import numba
+    import networkx
+
+    print('scikit-learn: ', sklearn.__version__)
+    print('Numba:        ', numba.__version__)
+    print('NetworkX:     ', networkx.__version__)
+except:
+    pass
