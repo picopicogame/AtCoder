8f8a9ca0070588ed5c1c03233ef358747238a8bc
picopicogame <picopico_famicom@yahoo.co.jp>
2021-10-18 23:04:12 +0900

    BFSが一つ終わったのでひとまずコミット

--- participated/ABC/ABC223/D.py
+++ participated/ABC/ABC223/D.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from collections import deque
+import heapq
+
+"""参考:https://atcoder.jp/contests/abc223/submissions/26660061"""
 
 # 再帰のリミットをあげる
 sys.setrecursionlimit(10**6)
@@ -15,14 +19,51 @@ def resolve():
     graph = [[] for _ in range(n)]
     for _ in range(m):
         a, b = map(int, input().split())
-        graph[a - 1].append(b - 1)
-    print(graph)  # [[2, 3, 5], ..., [1, 3, 4]]
+        graph[b - 1].append(a - 1)
 
-    full_graph = [[]]
+    for i in range(n):
+        heapq.heapify(graph[i])
 
-    for i in range(m):
-        for j in range(m):
-            graph[i][j]
+    key = [i for i in range(n)]
+
+    graph_dic = dict(zip(key,graph))
+    temp_que = deque()
+    ans_li = []
+
+    for j in range(n):
+    #while temp_que:
+        for i in graph_dic.keys():
+            if len(graph_dic[i]) == 0:
+                temp_que.append(i)
+                break
+
+
+        if len(temp_que) == 0:
+            break
+
+        pop_index = temp_que.popleft()
+        ans_li.append(pop_index)
+        graph_dic.pop(pop_index)
+
+        for vertex_list in graph_dic:
+            #if len(graph_dic[vertex_list]) == 0:
+             #   continue
+            #heapq.heappop(graph_dic[vertex_list])
+            #print(graph_dic[vertex_list])
+
+            graph_dic[vertex_list] = [s for s in graph_dic[vertex_list] if s !=pop_index ]
+
+
+
+
+
+    if len(ans_li) != n:
+        print("-1")
+        exit()
+
+    for i, ans in enumerate(ans_li):
+        ans_li[i] += 1
+    print(*ans_li)
 
 
 if __name__ == "__main__":
--- syouzin/ant_book/2-1Full_Search/BFS/ABC007_C.py
+++ syouzin/ant_book/2-1Full_Search/BFS/ABC007_C.py
@@ -17,11 +17,13 @@ def resolve():
     gy, gx = map(int, input().split())
     maze = [list(input().rstrip()) for _ in range(r)]
     INF = float('inf')
-    distance_temp = [INF] * c
-    distance = []
-    for i in range(r):
-        distance.append(distance_temp)
 
+    distance = [[INF] * c for _ in range(r)]
+
+    sx -= 1
+    sy -= 1
+    gx -= 1
+    gy -= 1
     # 移動4方向のベクトル
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
@@ -32,8 +34,7 @@ def resolve():
         posx_que.append(sx)
         posy_que.append(sy)
         distance[sx][sy] = 0
-        print(sx, sy)
-        print(distance[sx][sy])
+
         count = 0
         while posx_que:
             pos_x = posx_que.popleft()
@@ -43,17 +44,22 @@ def resolve():
                 break
             # 移動4方向
             for ii in range(4):
+                #print(ii)
                 nx = pos_x + dx[ii]
                 ny = pos_y + dy[ii]
-                if 0 <= nx < c and 0 <= ny < r and maze[nx][ny] != "#" and distance[nx][ny] == INF:
+                #print("nx: " + str(nx))
+                #print("ny: " + str(ny))
+                #print(maze[nx][ny], distance[nx][ny])
+
+                if 0 <= nx < c and 0 <= ny < r and maze[ny][nx] != "#" and distance[ny][nx] == INF:
+
                     posx_que.append(nx)
                     posy_que.append(ny)
-                    distance[nx][ny] = distance[pos_x][pos_y] + 1
+                    #print(posx_que, posy_que)
+                    distance[ny][nx] = distance[pos_y][pos_x] + 1
+                    count += 1
 
-            count += 1
-
-        print(count)
-        return distance[gx][gy]
+        return distance[gy][gx]
 
 
 
