d9afdf36e64764126824ef1b18529035799030c6
picopicogame <picopico_famicom@yahoo.co.jp>
2021-11-03 21:45:58 +0900

    11月2日精進分

--- syouzin/ant_book/2-3Dynamic_Programming/TDPC_A.py
+++ syouzin/ant_book/2-3Dynamic_Programming/TDPC_A.py
@@ -15,10 +15,19 @@ def resolve():
     p_li = list(map(int, input().split()))
     M = 100
 
+    # dp[i][j] i番目まで見てj点を取ることが可能か格納した配列
     dp = [[0] * (n * M + 1) for _ in range(n+1)]
-    print(p_li)
 
+    # 0点は必ずある
+    dp[0][0] = 1
 
+    for i, a in enumerate(p_li):
+        for j in range(n * M +1):
+            dp[i+1][j] = dp[i][j]
+            if j >= a:
+                dp[i+1][j] |= dp[i][j-a]
+
+    print(sum(dp[n]))
 
 
 if __name__ == "__main__":
